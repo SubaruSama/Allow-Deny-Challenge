738948945bf8a7a5310cfd76c79eedb11143db51
SubaruSama <andresfauque@gmail.com>
2026-02-23 23:49:02 -0300

    Movendo para um arquivo que tenha as variáveis constantes.
    TIP: será que não ficaria melhor em um arquivo json, yaml, etc?

---
+++ bouncer/constants.py
@@ -0,0 +1,7 @@
+allowed_schemes = ["http", "https", "ftp"]
+
+# For future use, use those options to check and create lists and not hardcoding
+# allowed_kind = ["allowlist", "denylist"]
+
+allowlist_file = "\\bouncer\\data\\allowlist.txt"
+denylist_file = "\\bouncer\\data\\denylist.txt"
