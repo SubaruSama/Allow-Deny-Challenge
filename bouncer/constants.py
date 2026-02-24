805b5a476f1adee073252885bf6781036ae185fd
SubaruSama <andresfauque@gmail.com>
2026-02-24 17:25:00 -0300

    Organizando
    Agora no arquivo de consts possui uma funcao e files.

--- bouncer/constants.py
+++ bouncer/constants.py
@@ -1,7 +1,11 @@
-allowed_schemes = ["http", "https", "ftp"]
+def get_allowed_schemes() -> tuple[str, str, str]:
+    return ("http", "https", "ftp")
+
+
+files: dict[str, str] = {
+    "allowlist": "\\bouncer\\data\\allowlist.txt",
+    "denylist": "\\bouncer\\data\\denylist.txt",
+}
 
 # For future use, use those options to check and create lists and not hardcoding
 # allowed_kind = ["allowlist", "denylist"]
-
-allowlist_file = "\\bouncer\\data\\allowlist.txt"
-denylist_file = "\\bouncer\\data\\denylist.txt"
